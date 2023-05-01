dbf4359d5839972bde59b1ae11a2b49d22ca7dd5
tlyyn <115135843+tlyyn@users.noreply.github.com>
2023-05-01 21:35:23 +0300

    main

--- AP_Lab12.4rec/Source.cpp
+++ AP_Lab12.4rec/Source.cpp
@@ -1,74 +1,71 @@
 #include <iostream>
 
+typedef int Info;
 struct Elem {
     Elem* link;
-    int info;
+    Info info;
 };
 
-void insert(Elem*& L, int value) {
-    Elem* tmp = new Elem{ NULL, value };
-    if (L == NULL) {
-        L = tmp;
-        L->link = L;
+void insert(Elem*& L, Info value) {
+    Elem* tmp = new Elem;
+    tmp->info = value;
+    if (L != NULL) {
+        Elem* T = L;
+        while (T->link != L)
+            T = T->link;
+        T->link = tmp;
     }
     else {
-        Elem* p = L;
-        while (p->link != L) p = p->link;
-        p->link = tmp;
-        tmp->link = L;
+        L = tmp;
     }
+    tmp->link = L;
 }
 
-int remove(Elem*& L) {
-    if (L == NULL) return 0;
-    int value = L->info;
-    Elem* toDelete = L;
-    if (L->link == L) {
-        L = NULL;
+Info remove(Elem*& L) {
+    Elem* T = L;
+    while (T->link != L)
+        T = T->link;
+    Info value = L->info;
+    if (T != L) {
+        Elem* tmp = L->link;
+        delete L;
+        L = tmp;
+        T->link = L;
     }
     else {
-        Elem* p = L;
-        while (p->link != L) p = p->link;
-        p->link = L->link;
-        L = L->link;
+        delete L;
+        L = NULL;
     }
-    delete toDelete;
     return value;
 }
 
-
-void printList(Elem* L, Elem* end) {
-    if (L == NULL) return;
-    std::cout << L->info << " ";
-    if (L->link != end)
-        printList(L->link, end);
+int CountNegative(Elem* first, Elem* L, int count = 0) {
+    if (L == NULL)
+        return count;
+    if (L->info < 0)
+        count++;
+    if (L->link != first)
+        return CountNegative(first, L->link, count);
+    else
+        return count;
 }
 
-int countNegative(Elem* L, Elem* end) {
-    if (L == NULL) return 0;
-    if (L == end) return (L->info < 0);
-    return (L->info < 0) + countNegative(L->link, end);
+void insertAfter(Elem*& L, Info value) {
+    Elem* tmp = new Elem;
+    tmp->info = value;
+    tmp->link = L->link;
+    L->link = tmp;
 }
 
 int main() {
     Elem* L = NULL;
-
-    insert(L, -1);
-    insert(L, 2);
-    insert(L, -3);
-    insert(L, 4);
-    insert(L, -5);
-    insert(L, 6);
-
-    std::cout << "List: ";
-    printList(L->link, L);
-
-    int negativeCount = countNegative(L->link, L);
-
-    std::cout << "\nNumber of negative elements: " << negativeCount << std::endl;
-
-    while (L != NULL)
-        remove(L);
-
+    insert(L, 1);
+    insert(L, -2);
+    insert(L, 3);
+    insert(L, -4);
+    insert(L, 5);
+    int count = CountNegative(L, L);
+    std::cout << "List: " << "1, -2, 3, -4, 5" << std::endl;
+    std::cout << "Number of list items with negative information values: " << count << std::endl;
     return 0;
 }
--- UnitTest2/UnitTest2.cpp
+++ UnitTest2/UnitTest2.cpp
@@ -1,79 +1,23 @@
-#include <iostream>
-#include <cassert>
+#include "../AP_Lab12.4rec/Source.cpp" // assuming the given code is in a file named "code.h"
+#include "CppUnitTest.h"
 
-struct Elem {
-    Elem* link;
-    int info;
-};
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-void insert(Elem*& L, int value) {
-    Elem* tmp = new Elem{ NULL, value };
-    if (L == NULL) {
-        L = tmp;
-        L->link = L;
-    }
-    else {
-        Elem* p = L;
-        while (p->link != L) p = p->link;
-        p->link = tmp;
-        tmp->link = L;
-    }
-}
-
-int remove(Elem*& L) {
-    if (L == NULL) return 0;
-    int value = L->info;
-    Elem* toDelete = L;
-    if (L->link == L) {
-        L = NULL;
-    }
-    else {
-        Elem* p = L;
-        while (p->link != L) p = p->link;
-        p->link = L->link;
-        L = L->link;
-    }
-    delete toDelete;
-    return value;
-}
-
-void printList(Elem* L, Elem* end) {
-    if (L == NULL) return;
-    std::cout << L->info << " ";
-    if (L->link != end)
-        printList(L->link, end);
-}
-
-int countNegative(Elem* L, Elem* end) {
-    if (L == NULL) return 0;
-    if (L == end) return (L->info < 0);
-    return (L->info < 0) + countNegative(L->link, end);
-}
-
-void test() {
-    Elem* L = NULL;
-
-    insert(L, -1);
-    insert(L, 2);
-    insert(L, -3);
-    insert(L, 4);
-    insert(L, -5);
-    insert(L, 6);
-
-    assert(countNegative(L->link, L) == 3);
-
-    assert(remove(L) == -1);
-    assert(remove(L) == 2);
-    assert(remove(L) == -3);
-    assert(remove(L) == 4);
-    assert(remove(L) == -5);
-    assert(remove(L) == 6);
-
-    assert(L == NULL);
-
-    std::cout << "All tests passed!\n";
-}
-
-int main() {
-    test();
+namespace LinkedListUnitTest
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(test_CountNegative)
+        {
+            Elem* L = NULL;
+            insert(L, 1);
+            insert(L, -2);
+            insert(L, 3);
+            insert(L, -4);
+            insert(L, 5);
+            int count = CountNegative(L, L);
+            Assert::AreEqual(count, 2);
+        }
+    };
 }
